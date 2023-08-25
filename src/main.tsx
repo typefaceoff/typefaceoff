7758a1a8053ebf15a957422be124c7768a72ba29
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-08-25 15:00:24 +1200

    🧱 Colors and reset stylsheets.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './styles/reset.css';
 import './index.css';
+import './styles/colors.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
