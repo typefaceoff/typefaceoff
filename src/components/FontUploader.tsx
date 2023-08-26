a8694bf63aa22b07c135281406848a8a81897f62
Trevor <tpo920@aucklanduni.ac.nz>
2023-08-26 14:59:37 +1200

    Implemented drop-zone

--- src/components/FontUploader.tsx
+++ src/components/FontUploader.tsx
@@ -1,53 +1,48 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import '../styles/FontUploader.css';
+import { useDropzone } from 'react-dropzone';
 
 const FontUploader: React.FC<{ onFontSelected: (selectedFont: File | null) => void }> = ({
   onFontSelected,
 }) => {
   const [, setSelectedFont] = useState<File | null>(null);
   const [, setFontPreview] = useState<string | null>(null);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0];
-      setSelectedFont(file);
-      onFontSelected(file);
-      setFontPreview(URL.createObjectURL(file));
-    }
-  };
-
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const files = Array.from(e.dataTransfer.files);
-
+  
+  const onDrop = useCallback((acceptedFiles: Array<File>) => {
     const allowedExtensions = ['.otf', '.ttf', '.woff', '.woff2'];
-    const fontFiles = files.filter((file) => {
+    const fontFiles = acceptedFiles.filter((file) => {
       const extension = file.name.split('.').pop()?.toLowerCase();
       return allowedExtensions.includes(`.${extension}`);
     });
-
     if (fontFiles.length > 0) {
       const selectedFile = fontFiles[0];
       setSelectedFont(selectedFile);
       onFontSelected(selectedFile);
       setFontPreview(URL.createObjectURL(selectedFile));
     }
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive, isFocused } = useDropzone({ onDrop, maxFiles: 1});
+
+  const focusedStyle = {
+    borderColor: '#2196f3'
   };
 
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
+  const style = useMemo(() => ({
+    ...(isFocused ? focusedStyle : {}),
+  }), [
+    isFocused,
+  ]);
 
   return (
     <form>
-      <div className="drop-area" onDrop={handleDrop} onDragOver={handleDragOver}>
-        <p>Drag and drop a font file here</p>
-        <input
-          className="input-button"
-          type="file"
-          accept=".otf, .ttf, .woff, .woff2"
-          onChange={handleFileChange}
-        />
+      <div {...getRootProps({ className: 'drop-area', style})}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop here...</p> :
+            <p>Drag and drop a font file here</p>
+        }
       </div>
     </form>
   );
