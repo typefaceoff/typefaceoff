be335e58bd69fbe317966f846380fa258ae49d23
joaomad247 <joao.madelino@gmail.com>
2023-08-27 01:05:55 +1200

    Removed extra unused class from ProofHalfWidthLine component

--- src/components/ProofHalfWidthLine.tsx
+++ src/components/ProofHalfWidthLine.tsx
@@ -3,7 +3,7 @@ import '../styles/ProofHalfWidthLine.css';
 
 export default function HalfWidthLine(proofingText: string, pointSize: number, lineHeight: number) {
   return (
-    <div className="half-width-line-container">
+    <div>
       {SizingToken(pointSize, pointSize * lineHeight)}
       <p className="half-width-line" style={{ fontSize: pointSize, lineHeight: lineHeight }}>
         {proofingText}
