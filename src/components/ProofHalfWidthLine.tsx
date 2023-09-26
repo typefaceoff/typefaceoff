d399d93ffc27c711d1d732d4d0445b2a21b62cf6
Michelle <mwan391@aucklanduni.ac.nz>
2023-09-26 22:36:30 +1300

    refactor - use more meaningful function name

--- src/components/ProofHalfWidthLine.tsx
+++ src/components/ProofHalfWidthLine.tsx
@@ -2,12 +2,13 @@ import SizingToken from './SizingToken';
 import '../styles/ProofHalfWidthLine.css';
 
 export default function HalfWidthLine(proofingText: string, pointSize: number, lineHeight: number) {
-  const handleInput = (e: React.FormEvent<HTMLParagraphElement>) => {
+  const updateProofingText = (e: React.FormEvent<HTMLParagraphElement>) => {
     const all = document.getElementsByClassName('proof');
     for (const elem of all) {
       elem.textContent = e.currentTarget.textContent;
     }
   };
+
 
   return (
     <div>
@@ -15,7 +16,7 @@ export default function HalfWidthLine(proofingText: string, pointSize: number, l
       <p
         className="half-width-line proof"
         contentEditable
-        onInput={handleInput}
+        onInput={updateProofingText}
         style={{ fontSize: pointSize, lineHeight: lineHeight, maxHeight: 100 * lineHeight }}
       >
         {proofingText}
