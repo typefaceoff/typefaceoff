8b39852cfc1a7e0aad48cb708720c0efac19ce30
Michelle <mwan391@aucklanduni.ac.nz>
2023-09-26 19:30:45 +1300

    refactor - handler to proofing element text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -29,6 +29,15 @@ function App() {
     setSelectedFontRight(selectedFont);
   };
 
+  // Event handler to set a common text for all proof elements
+  const setCommonText = () => {
+    const all = document.getElementsByClassName('proof');
+    for (const elem of all) {
+      elem.textContent = `There was nothing so very remarkable in that; nor did Alice think it so very much out \n of the way to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.`;
+    }
+  };
+
+
   return (
     <div className="app">
       <header>
@@ -36,12 +45,7 @@ function App() {
         <p className="subtitle">Get started by dropping two fonts</p>
         <button
           className="button"
-          onClick={() => {
-            const all = document.getElementsByClassName('proof');
-            for (const elem of all) {
-              elem.textContent = `There was nothing so very remarkable in that; nor did Alice think it so very much out \n of the way to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.`;
-            }
-          }}
+          onClick={setCommonText}
         >
           Alice in Wonderland
         </button>
