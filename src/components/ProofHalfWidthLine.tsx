73050466ce7629840af8c8ce0da5d230ff9113cc
Michelle <mwan391@aucklanduni.ac.nz>
2023-09-26 22:28:56 +1300

    refactor - extract handleInput handler function

--- src/components/ProofHalfWidthLine.tsx
+++ src/components/ProofHalfWidthLine.tsx
@@ -2,18 +2,20 @@ import SizingToken from './SizingToken';
 import '../styles/ProofHalfWidthLine.css';
 
 export default function HalfWidthLine(proofingText: string, pointSize: number, lineHeight: number) {
+  const handleInput = (e: React.FormEvent<HTMLParagraphElement>) => {
+    const all = document.getElementsByClassName('proof');
+    for (const elem of all) {
+      elem.textContent = e.currentTarget.textContent;
+    }
+  };
+
   return (
     <div>
       {SizingToken(pointSize, pointSize * lineHeight)}
       <p
         className="half-width-line proof"
         contentEditable
-        onInput={(e) => {
-          const all = document.getElementsByClassName('proof');
-          for (const elem of all) {
-            elem.textContent = e.currentTarget.textContent;
-          }
-        }}
+        onInput={handleInput}
         style={{ fontSize: pointSize, lineHeight: lineHeight, maxHeight: 100 * lineHeight }}
       >
         {proofingText}
