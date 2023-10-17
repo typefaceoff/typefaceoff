a0eda71b4f788e8b8eeeafe85e4889ef5743df70
Gauri <gauriraj.nz@gmail.com>
2023-10-18 00:24:32 +1300

    Removed unused import

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
 import React from 'react';
-import FontTextPlaceholders from './FontTextPlaceholders';
 import postcss from 'postcss';
 import ArticleTextPlaceholders from './ArticleTextPlaceHolders';
 
