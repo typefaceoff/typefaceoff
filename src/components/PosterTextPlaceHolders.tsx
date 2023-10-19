e28016f52825ade3ef5cc85345013f56799f834c
rhun558 <rhun558@aucklanduni.ac.nz>
2023-10-19 16:25:20 +1300

    Removed unneeded import

--- src/components/PosterTextPlaceHolders.tsx
+++ src/components/PosterTextPlaceHolders.tsx
@@ -1,5 +1,4 @@
 import '../styles/FontTextPlaceholders.css';
-import FullWidthLine from './ProofFullWidthLine';
 import HalfWidthLine from './ProofHalfWidthLine';
 import CenteredLine from './ProofCenteredLine';
 import {
