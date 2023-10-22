ea9b9fe892faae1ca01767e09429a48b07c13ef8
Michelle <mwan391@aucklanduni.ac.nz>
2023-10-22 17:54:15 +1300

    fix - sonar cloud detected redundant comment

--- src/components/TemplatePlaceHolders/ArticleTextPlaceHolders.tsx
+++ src/components/TemplatePlaceHolders/ArticleTextPlaceHolders.tsx
@@ -17,11 +17,8 @@ export default function ArticleTextPlaceholders({ lineHeight }: ArticleTextPlace
         {FullWidthLine(articleText, displayPointSizes[0], lineHeight, false)}
       </div>
       <div className="template-double-columns">
-        {/* {HalfWidthLine(articleTitle, displayPointSizes[1], lineHeight, false)}
-        {HalfWidthLine(articleSubTitle, displayPointSizes[2], lineHeight, false)} */}
         {TitleLine(articleTitle, displayPointSizes[1], lineHeight, false)}
       </div>
-
       <div className="template-single-column">
         {FullWidthLine(articleAuthorLine, displayPointSizes[3], 1.5, false)}
       </div>
